0cbd4d650ec3127949f0d75484eb08f0b0c423ad
Luis Mendez <col8rg@gmail.com>
2024-01-18 10:19:25 -0600

    "se cambio letra N por icono de nueva pagina"

---
+++ addmenu.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded",function() {
+   var menu = document.getElementById("idmenu");
+   // console.log(menu);
+   var nspan = document.createElement("span");
+   nspan.classList.add("ProseMirror-menuitem");
+    var nimg = document.createElement("img");
+   nimg.id = "npagina";
+   nimg.src = "img/nueva_pagina.png";
+   nimg.style.width ="20px";
+   nimg.classList.add("ProseMirror-icon");
+   nimg.title = ("NuevaPagina");
+   //  var nDiv = document.createElement("div");
+   //  nDiv.id = "npagina"
+   //  nDiv.classList.add("ProseMirror-icon");
+   //  nDiv.title = ("NuevaPagina");
+   //  var tdiv = document.createTextNode("N");
+   //  nDiv.appendChild(tdiv);
+    nspan.appendChild(nimg);
+   //  nspan.appendChild(nDiv);
+   menu.appendChild(nspan);
+
+var npagina = document.getElementById("npagina");
+
+npagina.addEventListener("click", function() {
+   alert('click a nueva pagina');
+})
+})
+
