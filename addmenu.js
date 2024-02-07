46c195aaddf257f15df98c091fdecbfc2696eb53
Luis Mendez <col8rg@gmail.com>
2024-02-07 16:31:16 -0600

    correccion de fin de pagina y manejo de eventos

--- addmenu.js
+++ addmenu.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded",function() {
+
    var editor = document.getElementById("dvEditor");
    var padre = editor.parentElement;
    padre.id = "dvPrincipal";
@@ -34,13 +35,13 @@ npagina.addEventListener("click", function() {
    imagenSeparador.id="img1";
    imagenSeparador.src = "img/FIN_PAGINA.png";
    imagenSeparador.style.width = "20px";
-   imagenSeparador.alt="Fin de la página";
-   imagenSeparador.classList.add("ProseMirror-separator");
+   imagenSeparador.alt="Fin de la pagina";
+   // imagenSeparador.classList.add("ProseMirror-separator");
    dveditor.appendChild(imagenSeparador);
    dveditor.innerHTML = dveditor.innerHTML + '<P>';
 
    var ultimoParrafo = dveditor.lastElementChild;
-   console.log(ultimoParrafo);
+   // console.log(ultimoParrafo);
    // Establece el foco en el último párrafo
    ultimoParrafo.focus();
 
@@ -52,8 +53,68 @@ npagina.addEventListener("click", function() {
    selection.removeAllRanges();
    selection.addRange(range);
    dveditor.focus();
+
+})//Fin evento Click en boton nueva pagina
+
+editor.addEventListener("keyup",function(eventoKey){
+   LimpiarNodoFinPagina();
+})
+editor.addEventListener("click",function(evento){
+
+   // evento.stopPropagation();
+   // evento.preventDefault();
+   for(var i = 0; i < evento.target.childNodes.length; i ++)
+   {
+      if(evento.target.childNodes[i].localName == "img" && evento.target.childNodes[i].alt == "Fin de la pagina")
+      {
+         var siguienteParrafo = (evento.target.nextSibling);
+         console.log(siguienteParrafo);
+         var seleccion = window.getSelection();
+         var rango = document.createRange();
+         if(siguienteParrafo != null) //si esto es nulo es porque no existe siguiente parrafo
+         {
+            rango.selectNodeContents(siguienteParrafo);
+            rango.collapse(false);
+            seleccion.removeAllRanges();
+            seleccion.addRange(rango);
+         }
+         else
+         {
+            var editor = document.getElementById("dvEditor");
+            editor.innerHTML = editor.innerHTML + "<P></P>";
+         }
+      }
+   }
    
+   // LimpiarNodoFinPagina(evento);
+   // 
 })
 
+function LimpiarNodoFinPagina()
+{
+   var editorLimpiar = document.getElementById("dvEditor");
+   for(var i = 0; i < editorLimpiar.childNodes.length; i ++)
+   {
+      if(editorLimpiar.childNodes[i].localName == "p")
+      {
+         var nodoDiv = editorLimpiar.childNodes[i];
+         for(var j = 0; j < nodoDiv.childNodes.length; j++)
+         {
+            if(nodoDiv.childNodes[j].localName == "img" && nodoDiv.childNodes[j].alt == "Fin de la pagina")
+            {
+               for (var k= 0; k < nodoDiv.childNodes.length; k++)
+               {
+                  if(nodoDiv.childNodes[k].localName != "br" && nodoDiv.childNodes[k].alt != "Fin de la pagina" )
+                     nodoDiv.removeChild(nodoDiv.childNodes[k]);
+                  // evento.target.removeChild(evento.target.childNodes[i]);
+               }
+               break;
+               //nos movemos al siguiente parrafo
+               // evento.target.contenEditable = false;
+            }
+         }
+      }
+   }
+}
 })
 
